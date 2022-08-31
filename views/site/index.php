6144871f45c544834294dcf8b36a567a1f9d9a40
Robertoraj <robertoraj76@gmail.com>
2022-08-31 14:14:42 -0300

    On branch main  Changes to be committed: 	new file:   controllers/MLivreController.php 	new file:   views/mlivre/index copy.php 	new file:   views/mlivre/index.php     modified:   views/site/index.php

--- views/site/index.php
+++ views/site/index.php
@@ -9,19 +9,26 @@ $this->title = 'Manutenção de Cargos e Funcionários';
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-4">
                 <h2>Cargos</h2>
 
                 <p>Lista dos cargos.</p>
 
                 <p><a class="btn btn-outline-secondary" href="/cargo/index">Manutenção</a></p>
             </div>
-            <div class="col-lg-6">
+            <div class="col-lg-4">
                 <h2>Funcionários</h2>
 
                 <p>Lista dos funcionários.</p>
 
                 <p><a class="btn btn-outline-secondary" href="/funcionario/index">Manutenção</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Mercado Livre</h2>
+
+                <p>Produtos Mercado Livre.</p>
+
+                <p><a class="btn btn-outline-secondary" href="/mlivre/index">Consultar</a></p>
             </div>
         </div>
 
